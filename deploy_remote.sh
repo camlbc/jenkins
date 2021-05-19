52051a16fe381b8a91ada9ed69c7d1aed642c735
camlbc <83454405+camlbc@users.noreply.github.com>
2021-05-19 15:45:59 +0200

    Update deploy_remote.sh

--- deploy_remote.sh
+++ deploy_remote.sh
@@ -1,14 +1,24 @@
-#!/bin/bash 
+#!/bin/bash
 #deploy_remote.sh
-java -version
-if [[$? !=0 ]] 
-then 
-  sudo amazon-linux-extras install -y java-openjdk11 
-fi
 
-#On copie les fichiers sur la machine distante
-scp ec2-user@ec2-3-17-183-87.us-east-2.compute.amazonaws.com
-helloWorld.class
+#Je mets la chaine de connexion de ma machine distance dans une variable
+MACHINE_DESTINATION="ec2-user@ec2-3-17-183-87.us-east-2.compute.amazonaws.com"
+PATH_SECRET_KEY="/secrets/kp-camille.pem"
 
-#On exécute l’application sur la machine distante
-ssh ec2-user@ec2-3-17-183-87.us-east-2.compute.amazonaws.com -c 'cd /app && java helloWorld'
+#On installe Java sur la machine distante 
+#ne pas oublier de mettre -o StrictHostKeyChecking=no pour ssh afin d'être non-interactif sur l'acception de la vérification de host
+#ne pas oublier de mettre -y pour yum ou amazon-linux-extras ou apt afin d'être non-interactif sur l'acception de l'installation
+ssh -o StrictHostKeyChecking=no -i $PATH_SECRET_KEY $MACHINE_DESTINATION 'sudo amazon-linux-extras install -y java-openjdk11'
+
+#On crée le répertoire /app sur la machine distante
+ssh -o StrictHostKeyChecking=no -i $PATH_SECRET_KEY $MACHINE_DESTINATION 'sudo mkdir -p /app && sudo chmod -R 777 /app && sudo chown ec2-user /app'
+
+#On copie le fichier .class dans le dossier homedir de l'utilisateur (car on n'arrive pas à l'envoyer directement sur /app)
+#possible de faire dans un dossier temporaire 
+scp -o StrictHostKeyChecking=no -i $PATH_SECRET_KEY helloWorld.class $MACHINE_DESTINATION:/home/ec2-user/helloWorld.class
+
+#On copie le fichier .class dans le dossier /app localement 
+ssh -o StrictHostKeyChecking=no -i $PATH_SECRET_KEY $MACHINE_DESTINATION 'sudo cp /home/ec2-user/helloWorld.class /app'
+
+#On execute le fichier helloWorld.class
+ssh -o StrictHostKeyChecking=no -i $PATH_SECRET_KEY $MACHINE_DESTINATION 'sudo cd /app && sudo java helloWorld'
